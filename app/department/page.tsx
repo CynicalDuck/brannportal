9257d3531c738c98519b0ac3eefd42b895d24dee
Marius Bekk <marius.bekk@intility.no>
2023-08-07 09:38:33 +0200

    Bugfix

--- app/department/page.tsx
+++ app/department/page.tsx
@@ -873,7 +873,7 @@ function Settings(data: any) {
       console.error("Error updating type name:", error.message);
     } else {
       // Create a new array by mapping over calloutTypes
-      const updatedCalloutTypes = calloutTypes.map((t) => {
+      const updatedCalloutTypes = calloutTypes.map((t: any) => {
         // If the type ID matches the updated type ID, return the modified type
         if (t.id === type.id) {
           return { ...t, value: value };
